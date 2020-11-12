e509a69826bd2ac1eeceac8fb083b587a3c47a1a
Bartek K <kontener00@gmail.com>
2020-11-12 19:41:17 +0100

    Added Homepage and Header

--- src/components/views/NotFound/NotFound.js
+++ src/components/views/NotFound/NotFound.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import Link from '@material-ui/core/Link';
+import Typography from '@material-ui/core/Typography';
 import clsx from 'clsx';
 
 // import { connect } from 'react-redux';
@@ -8,10 +10,13 @@ import clsx from 'clsx';
 
 import styles from './NotFound.module.scss';
 
-const Component = ({className, children}) => (
+const Component = ({ className, children }) => (
   <div className={clsx(className, styles.root)}>
-    <h2>NotFound</h2>
-    {children}
+    <Typography /* className={classes.root} */>
+      <Link href="/" /* onClick={preventDefault} */>
+        Go back to the homepage
+      </Link>
+    </Typography>
   </div>
 );
 

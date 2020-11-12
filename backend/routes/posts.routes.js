091d9219948b054b8ee9e710307faa29975cc614
Bartek K <kontener00@gmail.com>
2020-11-12 21:12:48 +0100

    Added SinglePost, EditPost, MyPost and backend scripts

--- backend/routes/posts.routes.js
+++ backend/routes/posts.routes.js
@@ -3,16 +3,25 @@ const router = express.Router();
 
 const Post = require('../models/post.model');
 
+const posts = require('../controllers/post.controller');
+
+router.get('/posts', posts.loadAll);
+router.get('/post/:id', posts.loadPostById);
+router.get('/my-posts', posts.loadByUser);
+router.post('/posts', posts.addPost);
+router.put('/post/:id', posts.editPost);
+
+
 router.get('/posts', async (req, res) => {
   try {
     const result = await Post
-      .find({status: 'published'})
+      .find({ status: 'published' })
       .select('author created title photo')
-      .sort({created: -1});
-    if(!result) res.status(404).json({ post: 'Not found' });
+      .sort({ created: -1 });
+    if (!result) res.status(404).json({ post: 'Not found' });
     else res.json(result);
   }
-  catch(err) {
+  catch (err) {
     res.status(500).json(err);
   }
 });
@@ -21,10 +30,10 @@ router.get('/posts/:id', async (req, res) => {
   try {
     const result = await Post
       .findById(req.params.id);
-    if(!result) res.status(404).json({ post: 'Not found' });
+    if (!result) res.status(404).json({ post: 'Not found' });
     else res.json(result);
   }
-  catch(err) {
+  catch (err) {
     res.status(500).json(err);
   }
 });
--- src/components/views/PostAdd/PostAdd.js
+++ src/components/views/PostAdd/PostAdd.js
@@ -4,12 +4,14 @@ import TextField from '@material-ui/core/TextField';
 import Container from '@material-ui/core/Container';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import PhotoCamera from '@material-ui/icons/PhotoCamera';
 import SaveIcon from '@material-ui/icons/Save';
+import PhotoCamera from '@material-ui/icons/PhotoCamera';
+import { NotFound } from '../../views/NotFound/NotFound';
 import { connect } from 'react-redux';
 import { addPostRequest } from '../../../redux/postsRedux';
 import { getUser } from '../../../redux/userRedux.js';
 import styles from './PostAdd.module.scss';
+import clsx from 'clsx';
 import { withRouter } from 'react-router-dom';
 
 class Component extends React.Component {
@@ -20,8 +22,9 @@ class Component extends React.Component {
         title: '',
         description: '',
         image: null,
+        email: '',
       },
-      error: null
+      isError: false
     };
   }
 
@@ -46,86 +49,114 @@ class Component extends React.Component {
 
   submitPost = async (e) => {
     const { post } = this.state;
-    const { addPost } = this.props;
+    const { addPost, user } = this.props;
 
     e.preventDefault();
-    if (post.title && post.description && post.image) {
-      await addPost(post);
-      this.setState({
-        post: {
-          title: '',
-          description: '',
-          image: null,
-        },
-        error: null
-      });
+    if (post.title && post.description && post.image && post.email) {
+      const formData = new FormData();
+
+      for (let key of ['email', 'description', 'title', 'location', 'phone']) {
+        formData.append(key, post[key]);
+      }
+
+      formData.append('image', post.image);
+      formData.append('user', user._id);
+
+      addPost(formData);
+      this.props.history.push('/my-posts');
     } else this.setState({ isError: true });
   };
 
 
   render() {
+    const { handleChange, setImage, submitPost } = this;
+    const { user, className } = this.props;
 
     return (
-      <Container className={styles.root}>
-        <Typography className={styles.title} variant="h5" color="textSecondary" component="h2">
-          Add new post
-        </Typography>
-        <form className={styles.form} noValidate autoComplete="off" onSubmit={this.submitPost}>
-          <div className={styles.formBox}>
-            <TextField
-              className={styles.formBox}
-              id="outlined-textarea"
-              label="Notice title"
-              placeholder="Notice title"
-              multiline
-              variant="outlined"
-              onChange={this.handleChange}
-            />
-          </div>
-        </form>
-        <form className={styles.form} noValidate autoComplete="off">
-          <div className={styles.formBox}>
-            <TextField
-              className={styles.formBox}
-              id="outlined-textarea"
-              label="Description"
-              placeholder="Description"
-              rows={4}
-              multiline
-              variant="outlined"
-              onChange={this.handleChange}
-            />
-          </div>
-        </form>
-        <form className={styles.buttons} noValidate autoComplete="off">
-          <input accept="image/*" className={styles.input} id="contained-button-file" type="file" />
-          <label
-            htmlFor="contained-button-file"
-            className={styles.label}
-            label="upload image"
-            onChange={this.setImage}
-          >
+      user.authenticated ? (
+        <Container className={clsx(className, styles.root)}>
+          <Typography className={styles.title} variant="h5" color="textSecondary" component="h2">
+            Add new post
+          </Typography>
+          <form className={styles.form} noValidate autoComplete="off" onSubmit={submitPost}>
+            <div className={styles.formBox}>
+              <TextField
+                className={styles.formBox}
+                id="outlined-textarea"
+                label="Notice title"
+                placeholder="Notice title"
+                multiline
+                variant="outlined"
+                onChange={handleChange}
+              />
+            </div>
+            <div className={styles.formBox}>
+              <TextField
+                className={styles.formBox}
+                id="outlined-textarea"
+                label="location"
+                placeholder="location"
+                multiline
+                variant="outlined"
+                onChange={handleChange}
+              />
+            </div>
+            <div className={styles.formBox}>
+              <TextField
+                className={styles.formBox}
+                id="outlined-textarea"
+                label="e-mail"
+                placeholder="e-mail"
+                multiline
+                variant="outlined"
+                onChange={handleChange}
+              />
+            </div>
+            <div className={styles.formBox}>
+              <TextField
+                className={styles.formBox}
+                id="outlined-textarea"
+                label="Description"
+                placeholder="Description"
+                rows={4}
+                multiline
+                variant="outlined"
+                onChange={handleChange}
+              />
+            </div>
+            <input accept="image/*" className={styles.input} id="contained-button-file" type="file" />
+            <label
+              htmlFor="contained-button-file"
+              className={styles.label}
+              label="upload image"
+              onChange={setImage}
+            >
+              <Button
+                color="primary"
+                className={styles.button}
+                aria-label="upload picture"
+                variant="contained"
+                startIcon={<PhotoCamera />}
+                component="span">
+                Upload image
+              </Button>
+            </label>
             <Button
-              color="primary"
-              aria-label="upload picture"
               variant="contained"
-              startIcon={<PhotoCamera />}
-              component="span"
-              size="large">
-              Upload image
+              className={styles.button}
+              color="secondary"
+              startIcon={<SaveIcon />}
+              type="submit"
+            >
+              Save
             </Button>
-          </label>
-          <Button
-            variant="contained"
-            color="secondary"
-            startIcon={<SaveIcon />}
-            size="large"
-          >
-            Save
-          </Button>
-        </form>
+          </form>
 
-      </Container>
+        </Container>
+      ) :
+        (
+          <NotFound />
+        )
     );
   }
 }
